55fb073157e221b5fa7845a808ffee48191cae99
groundkcm <karaminseo@naver.com>
2022-04-26 18:37:55 +0900

    preparation debugging

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
 import re
 import tkinter
+# 디버그 기능 추가 이미 파일있으면, 학기와 강의이름 tkinter에서 입력받기
 
+# try:
+#     pass
+# except:
+#     pass
 
 my_lectures = ['3D 게임 프로그래밍', 'STL', '네트워크 기초', '선형대수학', '스크립터 언어', '인간과 철학']
 
